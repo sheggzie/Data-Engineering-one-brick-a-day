2c4eb6f124b1d85add6c1b683a6f30ecaab0deb4
sheggzie <sheggzie@gmail.com>
2025-12-24 23:17:52 -0600

    day 1 task done

---
+++ w1d1.py
@@ -0,0 +1,39 @@
+# import requests
+
+# base_url = "https://pokeapi.co/api/v2/"
+
+# def get_pokemon_info(name):
+#     url = f"{base_url}/pokemon/{name}"
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#         pokemon_data = response.json()
+#         return pokemon_data
+#     else:
+#         print(f"Failed to retrieve data {response.status_code}")
+
+# pokemon_name = "Umbreon"
+# pokemon_info = get_pokemon_info(pokemon_name)
+
+# if pokemon_info:
+#     print("Name", pokemon_info["name"].capitalize())
+#     print("ID", pokemon_info["id"])
+#     print("Height", pokemon_info["height"])
+#     print("Weight", pokemon_info["weight"])
+
+
+import requests
+import json
+
+base_url = "https://pokeapi.co/api/v2/pokemon"
+
+def get_pokemon_info():
+    response = requests.get(base_url)
+
+    response_json = json.loads(response.text)
+    
+    with open("pokemon_data", "w") as file:
+        data = json.dump(response_json, file)
+    print(data)
+    
+get_pokemon_info()
