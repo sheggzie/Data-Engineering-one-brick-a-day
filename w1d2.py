a3d8460a2b8e565305148fe665831f2bdd107999
sheggzie <sheggzie@gmail.com>
2025-12-24 23:36:00 -0600

    commit w1d1 task done

--- w1d2.py
+++ w1d2.py
@@ -1,2 +1,15 @@
 import requests
-import json 
+import json
+
+base_url = "https://pokeapi.co/api/v2/pokemon"
+
+def get_pokemon_info():
+    response = requests.get(base_url)
+
+    response_json = json.loads(response.text)
+    
+    with open("pokemon_data", "w") as file:
+        data = json.dump(response_json, file)
+    print(data)
+    
+get_pokemon_info()
