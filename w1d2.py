436f81588fd668ff5755b3735e62fb4cd6119cae
sheggzie <sheggzie@gmail.com>
2025-12-27 00:58:06 -0600

    w1d2 done

--- w1d2.py
+++ w1d2.py
@@ -9,7 +9,7 @@ data.to_csv('pokemon_data_table.csv')   # convert contents in data to csv
 
 # function to remove empty columns from csv file 
 def clean_csv():
-    df = pd.read_csv('pokemon_data_table.csv')
+    df = pd.read_csv('pokemon_data_table.csv') 
     df.drop(columns=['count', 'previous'], inplace=True) # drop/remove two columns as named in the array
 
     df.to_csv("pokemon_data_table_cleaned.csv", index=False) # rewrite cleaned csv to new file
