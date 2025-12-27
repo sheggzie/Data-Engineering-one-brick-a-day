e03b6ea4d3c7a897ad8a5a2bcff1917c33e01b40
sheggzie <sheggzie@gmail.com>
2025-12-27 00:37:34 -0600

    w1d3 done

--- w1d2.py
+++ w1d2.py
@@ -3,5 +3,15 @@ import pandas as pd
 
 json_file = "pokemon_data.json"
 
-data = pd.read_json(json_file)
-data.to_csv('pokemon_data_table.csv')
+
+data = pd.read_json(json_file)  # read json file contents and assign to data
+data.to_csv('pokemon_data_table.csv')   # convert contents in data to csv
+
+# function to remove empty columns from csv file 
+def clean_csv():
+    df = pd.read_csv('pokemon_data_table.csv')
+    df.drop(columns=['count', 'previous'], inplace=True) # drop/remove two columns as named in the array
+
+    df.to_csv("pokemon_data_table_cleaned.csv", index=False) # rewrite cleaned csv to new file
+        
+clean_csv()
