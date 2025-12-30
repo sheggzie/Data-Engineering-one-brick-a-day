828ff01dbdb8b4f9bf5b67dcb792ccfa0839ac84
sheggzie <sheggzie@gmail.com>
2025-12-29 23:43:38 -0600

    comments for w1d4

--- w1d4.py
+++ w1d4.py
@@ -1,11 +1,14 @@
-def make_odd(num):
-    if num%2==1:
+def make_odd(num): # function returns odd number regardless of number entered, if given number is even, it turns it to odd.
+    if num%2==1: # checks if entered number is even.
         print(f"num {num} is odd!")
         return True
     else:
-        return make_odd(num+1)
-entered_num = 6
-if entered_num % 2 == 0:
+        return make_odd(num+1) # adds one to given odd number to make it even and return output.
+
+entered_num = 6 # sample number to check for if even or odd.
+
+if entered_num % 2 == 0: # checks if entered number is even.
     print(f"number {entered_num} entered is even, adding 1 to make it {entered_num + 1} which is odd!")
+    
 print(make_odd(entered_num))
         
