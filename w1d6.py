ee67340604826927dcc9e9d9ebcc63a0ab8ef347
sheggzie <sheggzie@gmail.com>
2025-12-31 23:41:57 -0600

    getting crypto token data from CoinGecko via API

--- w1d6.py
+++ w1d6.py
@@ -1,4 +1,11 @@
 import requests
 import json
 
-url = 
+api_key = "CG-hfBEmf4GKtLd83FfP61qwDV9"
+coin_name = input("Enter coin name: ")
+
+base_url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coin_name}&x_cg_demo_api_key={api_key}"
+
+response = requests.get(base_url)
+data = json.loads(response.text)
+print(data)
