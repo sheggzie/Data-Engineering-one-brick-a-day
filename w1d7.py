ec1ef89d6537370f4ad79026ce7c89c99b292209
sheggzie <sheggzie@gmail.com>
2026-01-03 23:20:56 -0600

    practice and added comments

--- w1d7.py
+++ w1d7.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-data = pd.read_json("bitcoin_data.json")
-flip = data.to_csv("bitcoin_data.csv")
+data = pd.read_json("bitcoin_data.json") # reads json data into pandas
+flip = data.to_csv("bitcoin_data.csv") # converts json data into CSV file format
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data) # make a table with rows and column from data
 
-token_symbol = df["symbol"]
-print(token_symbol)
+# token_symbol = df["symbol"]
+
+token_id = df.iloc[0,0] # prints bitcoin
+
+
+print(token_id)
